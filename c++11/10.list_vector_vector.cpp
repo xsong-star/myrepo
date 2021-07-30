71a2032c64d4fbac852c2d88f1770f5344dbaceb
xsong <18810210513@163.com>
2021-07-30 10:42:02 +0800

    Enter

---
+++ c++11/10.list_vector_vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //c++11标准
+    vector<vector<int>> res1;
+    //c++11之前的标准
+    vector<vector<int> > res2;
+
+}
